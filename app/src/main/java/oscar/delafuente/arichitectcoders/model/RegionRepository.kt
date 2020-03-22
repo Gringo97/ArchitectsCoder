e00e3b2e9336edde6d1e3374500aa0cda6433d98
OscarDLFR97 <oscar.delafuenteruiz@plexus.es>
2020-03-22 18:20:25 +0100

    Implement SOLID on the MainActivity, also clear code.

---
+++ app/src/main/java/oscar/delafuente/arichitectcoders/model/RegionRepository.kt
@@ -0,0 +1,31 @@
+package oscar.delafuente.arichitectcoders.model
+
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.app.Activity
+import android.location.Geocoder
+import android.location.Location
+
+class RegionRepository(activity: Activity) {
+
+    companion object {
+        private const val DEFAULT_REGION = "US"
+    }
+
+    private val locationDataSource = PlayServicesLocationDataSource(activity)
+    private val coarsePermissionChecker = PermissionChecker(activity, ACCESS_COARSE_LOCATION)
+    private val geocoder = Geocoder(activity)
+
+    suspend fun findLastRegion(): String = findLastLocation().toRegion()
+
+    private suspend fun findLastLocation(): Location? {
+        val success = coarsePermissionChecker.request()
+        return if (success) locationDataSource.findLastLocation() else null
+    }
+
+    private fun Location?.toRegion(): String {
+        val addresses = this?.let {
+            geocoder.getFromLocation(latitude, longitude, 1)
+        }
+        return addresses?.firstOrNull()?.countryCode ?: DEFAULT_REGION
+    }
+}
--- app/src/main/java/oscar/delafuente/arichitectcoders/ui/Extensions.kt
+++ app/src/main/java/oscar/delafuente/arichitectcoders/ui/Extensions.kt
@@ -7,13 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import com.bumptech.glide.Glide
-
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = true): View =
     LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
