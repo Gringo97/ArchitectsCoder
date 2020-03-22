10a9a5c75c0c1d517838377346f688312ae3bc27
OscarDLFR97 <oscar.delafuenteruiz@plexus.es>
2020-03-22 18:39:54 +0100

    Implementation of the custom view for the movie detail (close #11)

---
+++ app/src/main/java/oscar/delafuente/arichitectcoders/ui/MovieDetailInfoView.kt
@@ -0,0 +1,34 @@
+package oscar.delafuente.arichitectcoders.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import oscar.delafuente.arichitectcoders.model.Movie
+
+class MovieDetailInfoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : androidx.appcompat.widget.AppCompatTextView(context, attrs, defStyleAttr) {
+
+    fun setMovie(movie: Movie) = with(movie) {
+        text = buildSpannedString {
+
+            bold { append("Original language: ") }
+            appendln(originalLanguage)
+
+            bold { append("Original title: ") }
+            appendln(originalTitle)
+
+            bold { append("Release date: ") }
+            appendln(releaseDate)
+
+            bold { append("Popularity: ") }
+            appendln(popularity.toString())
+
+            bold { append("Vote Average: ") }
+            append(voteAverage.toString())
+        }
+    }
+}
