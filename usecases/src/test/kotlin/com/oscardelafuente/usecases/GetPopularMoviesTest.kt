386bcbd6c2fffaaca507fefb4b2c54a42677aaa8
Oscar de la Funte Ruiz <oscar.delafuenteruiz@plexus.es>
2020-04-10 19:28:23 +0200

    Implementation of test on the data and usecases modules

---
+++ usecases/src/test/kotlin/com/oscardelafuente/usecases/GetPopularMoviesTest.kt
@@ -0,0 +1,39 @@
+package com.oscardelafuente.usecases
+
+
+import com.nhaarman.mockitokotlin2.whenever
+import com.oscardelafuente.data.repository.MoviesRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetPopularMoviesTest {
+
+    @Mock
+    lateinit var moviesRepository: MoviesRepository
+
+    private lateinit var getPopularMovies: GetPopularMovies
+
+    @Before
+    fun setUp() {
+        getPopularMovies = GetPopularMovies(moviesRepository)
+    }
+
+    @Test
+    fun `invoke calls movies repository`() {
+        runBlocking {
+
+            val movies = listOf(mockedMovie.copy(id = 1))
+            whenever(moviesRepository.getPopularMovies()).thenReturn(movies)
+
+            val result = getPopularMovies.invoke()
+
+            Assert.assertEquals(movies, result)
+        }
+    }
+}
